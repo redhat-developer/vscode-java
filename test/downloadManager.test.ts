cd177611bd5ca48a7917d5c80556b3d5de550f2a
Gorkem Ercan <gorkem.ercan@gmail.com>
2016-09-15 18:10:05 -0400

    Initial set of tests

---
+++ test/downloadManager.test.ts
@@ -0,0 +1,27 @@
+import * as assert from 'assert';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as vscode from 'vscode';
+import * as  downloadManager from '../src/downloadManager';
+
+
+suite("Java Language Extension downloadManager tests", () => {
+
+    // Defines a Mocha unit test
+    test("Download server", function(done ) {
+        this.timeout(2*60*1000);
+        return downloadManager.downloadAndInstallServer()
+            .then(() => {
+                let pluginsPath = path.resolve(__dirname, '../../server/plugins');
+                try {
+                    console.log(pluginsPath);
+                    let isDirectory = fs.lstatSync(pluginsPath);
+                    assert.ok(isDirectory,"plugins folder is not found");
+                }
+                catch (err) {
+                    assert.ifError(err);
+                }
+                done();
+            });
+    });
+});
--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,22 +1,17 @@
-// 
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as java from '../src/extension';
 
-// Defines a Mocha test suite to group tests of similar kind together
 suite("Java Language Extension Tests", () => {
 
-	// Defines a Mocha unit test
-	test("Something 1", () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
+	test("Check extension", () => {
+		assert.ok(vscode.extensions.getExtension('redhat.java'));
+	});
+
+	test("Activate extension", function(done){
+		 this.timeout(1*60*1000);
+		return vscode.extensions.getExtension('redhat.java').activate().then((api)=>{
+			done();
+		});
 	});
 });
