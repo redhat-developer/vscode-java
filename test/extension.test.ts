d641d1a063fb27db42cdc0d6af6fb5147fd2f6f9
Gorkem Ercan <gorkem.ercan@gmail.com>
2016-09-18 17:46:11 -0400

    Add a test for commands
    Adds a test that checks all the required commands
    are registered by the extension

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -2,15 +2,27 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as java from '../src/extension';
 
-suite("Java Language Extension Tests", () => {
+suite("Java Language Extension", () => {
 
-	test("Check extension", () => {
+	test("should be present", () => {
 		assert.ok(vscode.extensions.getExtension('redhat.java'));
 	});
 
-	test("Activate extension", function(done){
-		 this.timeout(1*60*1000);
-		return vscode.extensions.getExtension('redhat.java').activate().then((api)=>{
+	test("should activate", function (done) {
+		this.timeout(1 * 60 * 1000);
+		return vscode.extensions.getExtension('redhat.java').activate().then((api) => {
+			done();
+		});
+	});
+
+	test("should register all java commands", function (done) {
+		return vscode.commands.getCommands(true).then((commands) =>
+		{
+			let javaCmds = commands.filter(function(value){
+				return "java.open.output" === value || 
+						"java.show.references" === value;
+			});
+			assert.ok(javaCmds.length == 2, "missing java commands");
 			done();
 		});
 	});
