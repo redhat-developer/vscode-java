3c669b63faf0e52efa03c25c607597e4bc945671
Fred Bricon <fbricon@gmail.com>
2017-04-18 21:06:19 -0400

    Use Maven wrapper to build server
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const gulp_tslint = require('gulp-tslint');
 const cp = require('child_process');
 const decompress = require('gulp-decompress');
 const download = require('gulp-download');
-
+const server_dir = '../eclipse.jdt.ls';
 //...
 gulp.task('tslint', () => {
     return gulp.src(['**/*.ts', '!**/*.d.ts', '!node_modules/**'])
@@ -21,34 +21,47 @@ gulp.task('download_server', ()=>
 
 gulp.task('build_server', ()=>
 {
-	cp.execSync('mvn -f ../eclipse.jdt.ls/pom.xml -Pserver-distro clean package', {stdio:[0,1,2]} );
-	gulp.src('../eclipse.jdt.ls/org.eclipse.jdt.ls.product/distro/*.tar.gz')
+	cp.execSync(mvnw()+ ' -Pserver-distro clean package', {cwd:server_dir, stdio:[0,1,2]} );
+	gulp.src(server_dir + '/org.eclipse.jdt.ls.product/distro/*.tar.gz')
 		.pipe(decompress())
 		.pipe(gulp.dest('./server'))
 });
 
 gulp.task('dev_server', ()=>
 {
-	let isWin = /^win/.test(process.platform);
-	let isMac = /^darwin/.test(process.platform);
-	let isLinux = /^linux/.test(process.platform);
-	let command = 'mvn -f ../eclipse.jdt.ls/pom.xml -Pserver-distro,fast -o clean package ';
-	if(isLinux){
+	let command = mvnw() +' -Pserver-distro,fast -o clean package ';
+	if(isLinux()){
 		command +='-Denvironment.os=linux -Denvironment.ws=gtk -Denvironment.arch=x86_64';
 	}
-	if(isMac){
+	else if(isMac()){
 		command += '-Denvironment.os=macosx -Denvironment.ws=cocoa -Denvironment.arch=x86_64';
-	}else
-	if(isWin){
+	} 
+	else if(isWin()){
 		command += '-Denvironment.os=win32 -Denvironment.ws=win32 -Denvironment.arch=x86_64';
 	}
 	console.log('executing '+command);
-	cp.execSync(command, {stdio:[0,1,2]} );
-	gulp.src('../eclipse.jdt.ls/org.eclipse.jdt.ls.product/distro/*.tar.gz')
+	cp.execSync(command, {cwd:server_dir, stdio:[0,1,2]} );
+	gulp.src(server_dir +'/org.eclipse.jdt.ls.product/distro/*.tar.gz')
 		.pipe(decompress())
 		.pipe(gulp.dest('./server'))
 });
 
 gulp.task('watch_server',()=>{
-	gulp.watch('../eclipse.jdt.ls/org.eclipse.jdt.ls.core/**/*.java',['dev_server']);
-})
+	gulp.watch(server_dir+'/org.eclipse.jdt.ls.core/**/*.java',['dev_server']);
+});
+
+function isWin() {
+	return /^win/.test(process.platform);
+}
+
+function isMac() {
+	return /^darwin/.test(process.platform);
+}
+
+function isLinux() {
+	return /^linux/.test(process.platform);
+}
+
+function mvnw() {
+	return "./mvnw"+(isWin()?".cmd":"");
+}
