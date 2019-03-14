18fcbc222bca68dd660e889c35e71085c4c0a32e
Fred Bricon <fbricon@gmail.com>
2019-03-14 18:15:07 -0400

    Add status to ExtensionAPI
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- src/extension.api.ts
+++ src/extension.api.ts
@@ -2,5 +2,6 @@ import { RequirementsData } from './requirements';
 
 export interface ExtensionAPI {
     readonly apiVersion: string;
-    readonly javaRequirement: RequirementsData;
+	readonly javaRequirement: RequirementsData;
+	readonly status: "Started" | "Error";
 }
