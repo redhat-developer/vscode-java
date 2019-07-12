2a2fd988e1342c451327199713f540dc42f7b903
Jinbo Wang <jinbwan@microsoft.com>
2019-07-12 18:41:20 +0800

    Fix test case
    Signed-off-by: Jinbo Wang <jinbwan@microsoft.com>

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -52,7 +52,8 @@ suite('Java Language Extension', () => {
 				Commands.GENERATE_CONSTRUCTORS_PROMPT,
 				Commands.GENERATE_DELEGATE_METHODS_PROMPT,
 				Commands.APPLY_REFACTORING_COMMAND,
-				Commands.RENAME_COMMAND
+				Commands.RENAME_COMMAND,
+				Commands.MOVE_FILE_COMMAND
 			];
 			const foundJavaCommands = commands.filter((value) => {
 				return JAVA_COMMANDS.indexOf(value)>=0 || value.startsWith('java.');
