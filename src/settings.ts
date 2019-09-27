cab76747ae4b0aeef1abcb42294afd28715e2924
Nikolas Komonen <nikolaskomonen@gmail.com>
2019-09-27 18:40:52 -0400

    Pasting code into empty file  will trigger organize imports
    Issue #1075
    
    New preference: java.paste.organizeImportsOnPaste
    
    Signed-off-by: Nikolas Komonen <nikolaskomonen@gmail.com>

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { window, Uri, workspace, WorkspaceConfiguration, commands, ConfigurationTarget, env } from 'vscode';
+import { window, Uri, workspace, WorkspaceConfiguration, commands, ConfigurationTarget, env, ExtensionContext, TextEditor, Range, Disposable } from 'vscode';
 import { LanguageClient } from 'vscode-languageclient';
 import { Commands } from './commands';
 import { getJavaConfiguration } from './utils';
@@ -14,10 +14,13 @@ const changeItem = {
 };
 
 const EXCLUDE_FILE_CONFIG = 'configuration.checkProjectSettingsExclusions';
+export const ORGANIZE_IMPORTS_ON_PASTE = 'actionsOnPaste.organizeImports'; // java.actionsOnPaste.organizeImports
+// index of the paste disposable in the subscriptions array
+let pasteSubscriptionIndex;
 
 let oldConfig: WorkspaceConfiguration = getJavaConfiguration();
 
-export function onConfigurationChange() {
+export function onConfigurationChange(languageClient: LanguageClient, context: ExtensionContext) {
 	return workspace.onDidChangeConfiguration(params => {
 		if (!params.affectsConfiguration('java')) {
 			return;
@@ -30,14 +33,25 @@ export function onConfigurationChange() {
 			const msg = `Java Language Server configuration changed, please restart ${env.appName}.`;
 			const action = 'Restart Now';
 			const restartId = Commands.RELOAD_WINDOW;
-			oldConfig = newConfig;
 			window.showWarningMessage(msg, action).then((selection) => {
 				if (action === selection) {
 					commands.executeCommand(restartId);
 				}
 			});
 		}
+		handleJavaPasteConfigurationChange(languageClient, context, newConfig);
+		// update old config
+		oldConfig = newConfig;
 	});
+}
+
+/**
+ * Starting point for any settings that need to be handled when the server starts
+ * @param languageClient
+ * @param context
+ */
+export function initializeSettings(languageClient: LanguageClient, context: ExtensionContext) {
+	handleInitialConfigurations(languageClient, context, getJavaConfiguration());
 }
 
 export function excludeProjectSettingsFiles() {
@@ -113,4 +127,72 @@ export function getJavaEncoding(): string {
 		javaEncoding = config.get<string>('files.encoding', 'UTF-8');
 	}
 	return javaEncoding;
+}
+
+export function handleInitialConfigurations(languageClient: LanguageClient, context: ExtensionContext, newConfig: WorkspaceConfiguration) {
+	// organize imports on paste registration
+	if (newConfig.get(ORGANIZE_IMPORTS_ON_PASTE)) {
+		registerOverridePasteCommand(languageClient, context);
+	}
+}
+
+export function handleJavaPasteConfigurationChange(languageClient: LanguageClient, context: ExtensionContext, newConfig: WorkspaceConfiguration) {
+	const oldOrganizeImportsOnPaste = oldConfig.get(ORGANIZE_IMPORTS_ON_PASTE);
+	const newOrganizeImportsOnPaste = newConfig.get(ORGANIZE_IMPORTS_ON_PASTE);
+	if (oldOrganizeImportsOnPaste !== newOrganizeImportsOnPaste) {
+		if (newOrganizeImportsOnPaste === true) {
+			registerOverridePasteCommand(languageClient, context);
+		}
+		else {
+			unregisterOverridePasteCommand(languageClient, context);
+		}
+	}
+}
+
+export function registerOverridePasteCommand(languageClient: LanguageClient, context: ExtensionContext): void {
+    // referencing https://github.com/gazugafan/vscode-indent-on-paste/blob/master/src/extension.ts
+    const length = context.subscriptions.push(commands.registerCommand('editor.action.clipboardPasteAction', async () => {
+
+        const clipboardText: string = await env.clipboard.readText();
+        const editor: TextEditor = window.activeTextEditor;
+
+        const documentText: string = editor.document.getText();
+
+        const isCursorOnly = editor.selection.isEmpty;
+        let action: Thenable<boolean>;
+        if (isCursorOnly) {
+            action = editor.edit(textInserter => {
+                textInserter.insert(editor.selection.start, clipboardText);
+            });
+        }
+        else {
+            const start = editor.selection.start;
+            const end = editor.selection.end;
+            action = editor.edit(textInserter => {
+                textInserter.replace(new Range(start, end), clipboardText);
+            });
+        }
+
+        action.then((wasApplied) => {
+            const hasText: boolean = documentText !== null && /\S/.test(documentText);
+            const fileURI = editor.document.uri.toString();
+            if (wasApplied && !hasText && fileURI.endsWith(".java")) { // only organizing imports if document is blank
+                // Organize imports only requires uri from CodeActionParams
+                const codeActionParams = { textDocument: { uri: fileURI } };
+                commands.executeCommand(Commands.ORGANIZE_IMPORTS, codeActionParams);
+            }
+        });
+	}));
+
+	pasteSubscriptionIndex = length - 1;
+	languageClient.info(`Registered 'java.${ORGANIZE_IMPORTS_ON_PASTE}' command.`);
+}
+
+export function unregisterOverridePasteCommand(languageClient: LanguageClient, context: ExtensionContext) {
+	if (pasteSubscriptionIndex !== null) {
+		const pasteDisposable: Disposable = context.subscriptions[pasteSubscriptionIndex];
+		pasteDisposable.dispose();
+		pasteSubscriptionIndex = null;
+		languageClient.info(`Unregistered 'java.${ORGANIZE_IMPORTS_ON_PASTE}' command.`);
+	}
 }
