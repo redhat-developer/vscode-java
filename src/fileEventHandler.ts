88abc6f97b17abd03a2195d6b1ae979a4c05234b
Jinbo Wang <jinbwan@microsoft.com>
2020-03-31 11:46:57 +0800

    address review comments
    Signed-off-by: Jinbo Wang <jinbwan@microsoft.com>

--- src/fileEventHandler.ts
+++ src/fileEventHandler.ts
@@ -8,16 +8,16 @@ import { Commands } from './commands';
 
 let serverReady: boolean = false;
 
-export function setServerStatus(onReady: boolean) {
-    serverReady = onReady;
+export function setServerStatus(ready: boolean) {
+    serverReady = ready;
 }
 
 export function registerFileEventHandlers(client: LanguageClient, context: ExtensionContext, ) {
-    context.subscriptions.push(workspace.onDidCreateFiles(didCreateFiles));
+    context.subscriptions.push(workspace.onDidCreateFiles(handleNewJavaFiles));
 }
 
-async function didCreateFiles(e: FileCreateEvent) {
-    const emptyFiles: string[] = [];
+async function handleNewJavaFiles(e: FileCreateEvent) {
+    const emptyFiles: Uri[] = [];
     const textDocuments: TextDocument[] = [];
     for (const uri of e.files) {
         if (!uri.fsPath || !uri.fsPath.endsWith(".java")) {
@@ -29,7 +29,7 @@ async function didCreateFiles(e: FileCreateEvent) {
             continue;
         }
 
-        emptyFiles.push(uri.fsPath);
+        emptyFiles.push(uri);
         textDocuments.push(textDocument);
     }
 
@@ -46,19 +46,20 @@ async function didCreateFiles(e: FileCreateEvent) {
     }
 
     for (let i = 0; i < emptyFiles.length; i++) {
+        const packageName = resolvePackageName(sourcePaths, emptyFiles[i].fsPath);
+        const typeName: string = resolveTypeName(textDocuments[i].fileName);
         const snippets: string[] = [];
-        const packageName = resolvePackageName(sourcePaths, emptyFiles[i]);
         if (packageName) {
             snippets.push(`package ${packageName};`);
         }
-
-        const typeName: string = resolveTypeName(textDocuments[i].fileName);
-        snippets.push(...[
-            "",
-            `public \${1|class,interface,enum|} ${typeName} {`,
-            "",
-            "}"
-        ]);
+        snippets.push("");
+        if (!serverReady || await isVersionLessThan(emptyFiles[i].toString(), 14)) {
+            snippets.push(`public \${1|class,interface,enum|} ${typeName} {`);
+        } else {
+            snippets.push(`public \${1|class ${typeName},interface ${typeName},enum ${typeName},record ${typeName}()|} {`);
+        }
+        snippets.push("");
+        snippets.push("}");
         const textEditor = await window.showTextDocument(textDocuments[i]);
         textEditor.insertSnippet(new SnippetString(snippets.join("\n")));
     }
@@ -88,4 +89,33 @@ function resolvePackageName(sourcePaths: string[], filePath: string): string {
 function isPrefix(parentPath: string, filePath: string): boolean {
     const relative = path.relative(parentPath, filePath);
     return relative && !relative.startsWith('..') && !path.isAbsolute(relative);
+}
+
+const COMPLIANCE = "org.eclipse.jdt.core.compiler.compliance";
+async function isVersionLessThan(fileUri: string, targetVersion: number): Promise<boolean> {
+    let projectSettings = {};
+    try {
+        projectSettings = await commands.executeCommand<Object>(
+            Commands.EXECUTE_WORKSPACE_COMMAND, Commands.GET_PROJECT_SETTINGS, fileUri, [ COMPLIANCE ]);
+    } catch (err) {
+        // do nothing.
+    }
+
+    let javaVersion = 0;
+    let complianceVersion = projectSettings[COMPLIANCE];
+    if (complianceVersion) {
+        // Ignore '1.' prefix for legacy Java versions
+        if (complianceVersion.startsWith('1.')) {
+            complianceVersion = complianceVersion.substring(2);
+        }
+
+        // look into the interesting bits now
+        const regexp = /\d+/g;
+        const match = regexp.exec(complianceVersion);
+        if (match) {
+            javaVersion = parseInt(match[0]);
+        }
+    }
+
+    return javaVersion < targetVersion;
 }
