31c3969b18070de8ae5e56a85c3170044b7c608e
Yan Zhang <yanzh@microsoft.com>
2020-06-26 23:14:57 +0800

    support to resolve additional text edits
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- src/extension.ts
+++ src/extension.ts
@@ -169,7 +169,8 @@ export function activate(context: ExtensionContext): Promise<ExtensionAPI> {
 						moveRefactoringSupport: true,
 						clientHoverProvider: true,
 						clientDocumentSymbolProvider: true,
-						gradleChecksumWrapperPromptSupport: true
+						gradleChecksumWrapperPromptSupport: true,
+						resolveAdditionalTextEditsSupport: true
 					},
 					triggerFiles,
 				},
