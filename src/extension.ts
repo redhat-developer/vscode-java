5875958e26dd719c403f53a00947bb49f61c423a
Paul Cantrell <cantrell@pobox.com>
2020-08-29 11:09:51 -0500

    Punctuation fix in import message
    Signed-off-by: Paul Cantrell <cantrell@pobox.com>

--- src/extension.ts
+++ src/extension.ts
@@ -359,7 +359,7 @@ async function workspaceContainsBuildFiles(): Promise<boolean> {
 }
 
 async function promptUserForStandardServer(config: WorkspaceConfiguration): Promise<boolean> {
-	const choice: string = await window.showInformationMessage("The workspace contains Java projects, would you like to import them?", "Yes", "Always", "Later");
+	const choice: string = await window.showInformationMessage("The workspace contains Java projects. Would you like to import them?", "Yes", "Always", "Later");
 	switch (choice) {
 		case "Always":
 			await config.update("project.importOnFirstTimeStartup", "automatic", ConfigurationTarget.Global);
