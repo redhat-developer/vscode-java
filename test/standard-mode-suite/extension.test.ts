e9e504c0f51120037fddcbb3859b9397b6d6c183
Yan Zhang <yanzh@microsoft.com>
2020-09-28 16:20:10 +0800

    improve java version checking
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- test/standard-mode-suite/extension.test.ts
+++ test/standard-mode-suite/extension.test.ts
@@ -120,6 +120,7 @@ suite('Java Language Extension - Standard', () => {
 	test('should parse Java version', () => {
 		// Test boundaries
 		assert.equal(requirements.parseMajorVersion(null), 0);
+		assert.equal(requirements.parseMajorVersion(undefined), 0);
 		assert.equal(requirements.parseMajorVersion(''), 0);
 		assert.equal(requirements.parseMajorVersion('foo'), 0);
 		assert.equal(requirements.parseMajorVersion('version'), 0);
@@ -127,11 +128,11 @@ suite('Java Language Extension - Standard', () => {
 		assert.equal(requirements.parseMajorVersion('version "NaN"'), 0);
 
 		// Test the real stuff
-		assert.equal(requirements.parseMajorVersion('version "1.7"'), 7);
-		assert.equal(requirements.parseMajorVersion('version "1.8.0_151"'), 8);
-		assert.equal(requirements.parseMajorVersion('version "9"'), 9);
-		assert.equal(requirements.parseMajorVersion('version "9.0.1"'), 9);
-		assert.equal(requirements.parseMajorVersion('version "10-ea"'), 10);
+		assert.equal(requirements.parseMajorVersion('1.7'), 7);
+		assert.equal(requirements.parseMajorVersion('1.8.0_151'), 8);
+		assert.equal(requirements.parseMajorVersion('9'), 9);
+		assert.equal(requirements.parseMajorVersion('9.0.1'), 9);
+		assert.equal(requirements.parseMajorVersion('10-ea'), 10);
 	});
 
 	test('should detect debug flag', () => {
