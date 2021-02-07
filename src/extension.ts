aaac74db856c95370c6a34119d6face48fb5aba6
Vered Constantin <vered.constantin@sap.com>
2021-02-07 09:58:26 +0200

    [FIX] Fix java settings causes LSP issues in theia

--- src/extension.ts
+++ src/extension.ts
@@ -165,7 +165,7 @@ export function activate(context: ExtensionContext): Promise<ExtensionAPI> {
 				initializationOptions: {
 					bundles: collectJavaExtensions(extensions.all),
 					workspaceFolders: workspace.workspaceFolders ? workspace.workspaceFolders.map(f => f.uri.toString()) : null,
-					settings: { java: getJavaConfig(requirements.java_home) },
+					settings: getJavaConfig(requirements.java_home),
 					extendedClientCapabilities: {
 						progressReportProvider: getJavaConfiguration().get('progressReports.enabled'),
 						classFileContentsSupport: true,
@@ -192,9 +192,7 @@ export function activate(context: ExtensionContext): Promise<ExtensionAPI> {
 					workspace: {
 						didChangeConfiguration: () => {
 							standardClient.getClient().sendNotification(DidChangeConfigurationNotification.type, {
-								settings: {
-									java: getJavaConfig(requirements.java_home),
-								}
+								settings: getJavaConfig(requirements.java_home)
 							});
 						}
 					}
