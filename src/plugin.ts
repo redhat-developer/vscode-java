18b10b6baa4cbf6ce9f3e0e04fc721d0937997f5
Siarhei Leanavets <Siarhei_Leanavets1@epam.com>
2021-03-30 23:25:34 +0300

    Cache all the available patterns
    Signed-off-by: Siarhei Leanavets <Siarhei_Leanavets1@epam.com>

--- src/plugin.ts
+++ src/plugin.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 import { Commands } from './commands';
 
 let existingExtensions: Array<string>;
+let existingBuildFilePatterns: Array<string>;
 
 export function collectJavaExtensions(extensions: readonly vscode.Extension<any>[]): string[] {
 	const result = [];
@@ -26,7 +27,31 @@ export function collectJavaExtensions(extensions: readonly vscode.Extension<any>
 	return result;
 }
 
+export function collectBuildFilePattern(extensions: readonly vscode.Extension<any>[]) {
+	const result = [];
+	if (extensions && extensions.length) {
+		for (const extension of extensions) {
+			const contributesSection = extension.packageJSON['contributes'];
+			if (contributesSection) {
+				const buildFilePatterns = contributesSection['javaBuildFilePatterns'];
+				if (Array.isArray(buildFilePatterns) && buildFilePatterns.length) {
+					result.push(...buildFilePatterns);
+				}
+			}
+		}
+	}
+	// Make a copy of build file patterns:
+	existingBuildFilePatterns = result.slice();
+	return result;
+}
+
 export function onExtensionChange(extensions: readonly vscode.Extension<any>[]) {
+	updateExistingExtensions(extensions);
+	updateExistinguildFilePatterns(extensions);
+
+}
+
+function updateExistingExtensions(extensions: readonly vscode.Extension<any>[]) {
 	if (!existingExtensions) {
 		return;
 	}
@@ -52,4 +77,34 @@ export function onExtensionChange(extensions: readonly vscode.Extension<any>[]) 
 			}
 		});
 	}
+
+}
+
+function updateExistinguildFilePatterns(extensions: readonly vscode.Extension<any>[]) {
+	if (!existingBuildFilePatterns) {
+		return;
+	}
+	const oldPatterns = new Set(existingBuildFilePatterns.slice());
+	const newPatterns = collectBuildFilePattern(extensions);
+	let hasChanged = ( oldPatterns.size !== newPatterns.length);
+	if (!hasChanged) {
+		for (const newPattern of newPatterns) {
+			if (!oldPatterns.has(newPattern)) {
+				hasChanged = true;
+				break;
+			}
+		}
+	}
+
+	if (hasChanged) {
+		const msg = `Java build file patterns changed, reloading ${vscode.env.appName} is required for the changes to take effect.`;
+		const action = 'Reload';
+		const restartId = Commands.RELOAD_WINDOW;
+		vscode.window.showWarningMessage(msg, action).then((selection) => {
+			if (action === selection) {
+				vscode.commands.executeCommand(restartId);
+			}
+		});
+	}
+
 }
