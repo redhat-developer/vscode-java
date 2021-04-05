1a13ed84630f618ac9d169fddd4e665233e25afa
Siarhei Leanavets <Siarhei_Leanavets1@epam.com>
2021-04-05 22:58:08 +0300

    Make one pop-up dialog asking for reload.     Use regular expression to check java build file names.
        Signed-off-by: Siarhei Leanavets <Siarhei_Leanavets1@epam.com>
    
    Signed-off-by: Siarhei Leanavets <Siarhei_Leanavets1@epam.com>

--- src/plugin.ts
+++ src/plugin.ts
@@ -46,65 +46,46 @@ export function collectBuildFilePattern(extensions: readonly vscode.Extension<an
 }
 
 export function onExtensionChange(extensions: readonly vscode.Extension<any>[]) {
-	updateExistingExtensions(extensions);
-	updateExistinguildFilePatterns(extensions);
-
+	if (isExistingExtensionsUpdated(extensions) || isExistingBuildFilePatternsUpdated(extensions)) {
+		const msg = `Java Extension Contributions changed, reloading ${vscode.env.appName} is required for the changes to take effect.`;
+		const action = 'Reload';
+		const restartId = Commands.RELOAD_WINDOW;
+		vscode.window.showWarningMessage(msg, action).then((selection) => {
+			if (action === selection) {
+				vscode.commands.executeCommand(restartId);
+			}
+		});
+	}
 }
 
-function updateExistingExtensions(extensions: readonly vscode.Extension<any>[]) {
+function isExistingExtensionsUpdated(extensions: readonly vscode.Extension<any>[]) {
 	if (!existingExtensions) {
-		return;
+		return false;
 	}
 	const oldExtensions = new Set(existingExtensions.slice());
 	const newExtensions = collectJavaExtensions(extensions);
-	let hasChanged = ( oldExtensions.size !== newExtensions.length);
+	const hasChanged = ( oldExtensions.size !== newExtensions.length);
 	if (!hasChanged) {
 		for (const newExtension of newExtensions) {
 			if (!oldExtensions.has(newExtension)) {
-				hasChanged = true;
-				break;
+				return true;
 			}
 		}
 	}
-
-	if (hasChanged) {
-		const msg = `Extensions to the Java Language Server changed, reloading ${vscode.env.appName} is required for the changes to take effect.`;
-		const action = 'Reload';
-		const restartId = Commands.RELOAD_WINDOW;
-		vscode.window.showWarningMessage(msg, action).then((selection) => {
-			if (action === selection) {
-				vscode.commands.executeCommand(restartId);
-			}
-		});
-	}
-
 }
 
-function updateExistinguildFilePatterns(extensions: readonly vscode.Extension<any>[]) {
+function isExistingBuildFilePatternsUpdated(extensions: readonly vscode.Extension<any>[]) {
 	if (!existingBuildFilePatterns) {
-		return;
+		return false;
 	}
 	const oldPatterns = new Set(existingBuildFilePatterns.slice());
 	const newPatterns = collectBuildFilePattern(extensions);
-	let hasChanged = ( oldPatterns.size !== newPatterns.length);
+	const hasChanged = ( oldPatterns.size !== newPatterns.length);
 	if (!hasChanged) {
 		for (const newPattern of newPatterns) {
 			if (!oldPatterns.has(newPattern)) {
-				hasChanged = true;
-				break;
+				return true;
 			}
 		}
 	}
-
-	if (hasChanged) {
-		const msg = `Java build file patterns changed, reloading ${vscode.env.appName} is required for the changes to take effect.`;
-		const action = 'Reload';
-		const restartId = Commands.RELOAD_WINDOW;
-		vscode.window.showWarningMessage(msg, action).then((selection) => {
-			if (action === selection) {
-				vscode.commands.executeCommand(restartId);
-			}
-		});
-	}
-
 }
