532afa6394b7ae9ec31034968e29e1c1af5fdf3e
Jinbo Wang <jinbwan@microsoft.com>
2023-05-25 14:24:05 +0800

    Add null check for textEditor change listener
    Signed-off-by: Jinbo Wang <jinbwan@microsoft.com>

--- src/diagnostic.ts
+++ src/diagnostic.ts
@@ -6,7 +6,7 @@ import { validateAllOpenBuffersOnChanges } from "./settings";
 export function registerDocumentValidationListener(context: ExtensionContext, languageClient: LanguageClient) {
 	context.subscriptions.push(window.onDidChangeActiveTextEditor(textEditor => {
 		// Refresh the diagnostics when the focus is switched to a Java file.
-		if (textEditor.document.uri?.scheme === "file" && textEditor.document.languageId === "java") {
+		if (textEditor?.document.uri?.scheme === "file" && textEditor?.document.languageId === "java") {
 			if (!validateAllOpenBuffersOnChanges()) {
 				languageClient.sendNotification(ValidateDocumentNotification.type, {
 					textDocument: {
