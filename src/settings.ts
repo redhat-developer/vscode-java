3ad9b168dfc6db92907df2710fd1f5d61bfe49a1
Snjeza <snjezana.peco@redhat.com>
2023-07-24 22:38:43 +0200

    Smart semicolon autocorrect (#3159)
    - Introduce java.edit.smartSemicolonDetection.enabled
    
    Signed-off-by: Snjezana Peco <snjezana.peco@redhat.com>

--- src/settings.ts
+++ src/settings.ts
@@ -2,10 +2,12 @@
 
 import * as fs from 'fs';
 import * as path from 'path';
-import { commands, ConfigurationTarget, env, ExtensionContext, Position, Range, SnippetString, TextDocument, Uri, window, workspace, WorkspaceConfiguration, WorkspaceFolder } from 'vscode';
+import { commands, ConfigurationTarget, env, ExtensionContext, Position, Range, Selection, SnippetString, TextDocument, Uri, window, workspace, WorkspaceConfiguration, WorkspaceFolder } from 'vscode';
 import { Commands } from './commands';
 import { cleanupLombokCache } from './lombokSupport';
 import { ensureExists, getJavaConfiguration } from './utils';
+import { apiManager } from './apiManager';
+import { setSmartSemiColonDetectionState } from './smartSemicolonDetection';
 
 const DEFAULT_HIDDEN_FILES: string[] = ['**/.classpath', '**/.project', '**/.settings', '**/.factorypath'];
 const IS_WORKSPACE_JDK_ALLOWED = "java.ls.isJdkAllowed";
@@ -329,6 +331,9 @@ export function handleTextBlockClosing(document: TextDocument, changes: readonly
 		return;
 	}
 	if (lastChange.text !== '"""";') {
+		if (lastChange.text !== ';') {
+			setSmartSemiColonDetectionState(null, null);
+		}
 		return;
 	}
 	const selection = activeTextEditor.selection.active;
@@ -349,3 +354,5 @@ export function handleTextBlockClosing(document: TextDocument, changes: readonly
 		}
 	}
 }
+
+
