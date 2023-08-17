d02cf8ecfee1f3f528770a51ada825d522356967
Hope Hadfield <hhadfiel@redhat.com>
2023-08-17 10:50:29 -0400

    Support movement of snippets to JDT-LS
    Signed-off-by: Hope Hadfield <hhadfiel@redhat.com>

--- src/snippetCompletionProvider.ts
+++ src/snippetCompletionProvider.ts
@@ -56,7 +56,11 @@ class SnippetCompletionProviderImpl implements CompletionItemProvider {
 }
 
 export function beautifyDocument(raw: string): MarkdownString {
-    const escapedString = raw.replace(/\$\{\d:?(.*?)\}/gm, '$1').replace(/\$\d/gm, '');
+    const escapedString = raw.replace(/\$\{\d\|(.*?),.*?\}/gm, '$1')
+                            .replace(/\$\{\d:?(.*?)\}/gm, '$1')
+                            .replace(/\$\d/gm, '')
+                            .replace(/\${TM_SELECTED_TEXT:}/gm, '')
+                            .replace(/\${TM_FILENAME_BASE}/gm, '');
     return new MarkdownString().appendCodeblock(escapedString, "java");
 }
 
