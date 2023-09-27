ae058c7f9575818bc428ac1b2e52cac1b27e3225
fortinma <mathieu.m.fortin@autodesk.com>
2023-09-26 20:17:13 -0400

    Update to lombok 1.18.30

--- gulpfile.js
+++ gulpfile.js
@@ -127,7 +127,7 @@ gulp.task('download_lombok', async function (done) {
 	}
 
 	await new Promise(function (resolve, reject) {
-		const lombokVersion = '1.18.28';
+		const lombokVersion = '1.18.30';
 		// The latest lombok version can be found on the website https://projectlombok.org/downloads
 		const lombokUrl = `https://projectlombok.org/downloads/lombok-${lombokVersion}.jar`;
 		download(lombokUrl)
