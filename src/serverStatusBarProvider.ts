d05c5a2a64ef01a96c8bb1da24f6ef05c00c0c9c
Sheng Chen <sheche@microsoft.com>
2023-12-07 16:29:53 +0800

    Make the server status as a normal status bar item
    Signed-off-by: Sheng Chen <sheche@microsoft.com>

--- src/serverStatusBarProvider.ts
+++ src/serverStatusBarProvider.ts
@@ -1,123 +1,68 @@
 'use strict';
 
-import { StatusBarItem, window, StatusBarAlignment, version } from "vscode";
+import { StatusBarItem, window, StatusBarAlignment } from "vscode";
 import { Commands } from "./commands";
 import { Disposable } from "vscode-languageclient";
-import * as semver from "semver";
-import { ServerStatusItemFactory, StatusCommands, supportsLanguageStatus } from "./languageStatusItemFactory";
+import { StatusCommands } from "./languageStatusItemFactory";
 
 class ServerStatusBarProvider implements Disposable {
 	private statusBarItem: StatusBarItem;
-	private languageStatusItem: any;
-	// Adopt new API for status bar item, meanwhile keep the compatibility with Theia.
-	// See: https://github.com/redhat-developer/vscode-java/issues/1982
-	private isAdvancedStatusBarItem: boolean;
 
 	constructor() {
-		this.isAdvancedStatusBarItem = semver.gte(version, "1.57.0");
-	}
-
-	public initialize(): void {
-		if (supportsLanguageStatus()) {
-			this.languageStatusItem = ServerStatusItemFactory.create();
-		} else {
-			if (this.isAdvancedStatusBarItem) {
-				this.statusBarItem = (window.createStatusBarItem as any)("java.serverStatus", StatusBarAlignment.Right, Number.MIN_VALUE);
-			} else {
-				this.statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, Number.MIN_VALUE);
-			}
-		}
+		this.statusBarItem = window.createStatusBarItem("java.serverStatus", StatusBarAlignment.Right, Number.MIN_VALUE);
 	}
 
 	public showLightWeightStatus(): void {
-		if (supportsLanguageStatus()) {
-			ServerStatusItemFactory.showLightWeightStatus(this.languageStatusItem);
-		} else {
-			if (this.isAdvancedStatusBarItem) {
-				(this.statusBarItem as any).name = "Java Server Mode";
-			}
-			this.statusBarItem.text = StatusIcon.lightWeight;
-			this.statusBarItem.command = StatusCommands.switchToStandardCommand;
-			this.statusBarItem.tooltip = "Java language server is running in LightWeight mode, click to switch to Standard mode";
-			this.statusBarItem.show();
-		}
+		this.statusBarItem.name = "Java Server Mode";
+		this.statusBarItem.text = StatusIcon.lightWeight;
+		this.statusBarItem.command = StatusCommands.switchToStandardCommand;
+		this.statusBarItem.tooltip = "Java language server is running in LightWeight mode, click to switch to Standard mode";
+		this.statusBarItem.show();
 	}
 
 	public showNotImportedStatus(): void {
-		if (supportsLanguageStatus()) {
-			ServerStatusItemFactory.showNotImportedStatus(this.languageStatusItem);
-		} else {
-			if (this.isAdvancedStatusBarItem) {
-				(this.statusBarItem as any).name = "No projects are imported";
-			}
-			this.statusBarItem.text = StatusIcon.notImported;
-			this.statusBarItem.command = StatusCommands.startStandardServerCommand;
-			this.statusBarItem.tooltip = "No projects are imported, click to load projects";
-			this.statusBarItem.show();
-		}
+		this.statusBarItem.name = "No projects are imported";
+		this.statusBarItem.text = StatusIcon.notImported;
+		this.statusBarItem.command = StatusCommands.startStandardServerCommand;
+		this.statusBarItem.tooltip = "No projects are imported, click to load projects";
+		this.statusBarItem.show();
 	}
 
 	public showStandardStatus(): void {
-		if (supportsLanguageStatus()) {
-			ServerStatusItemFactory.showStandardStatus(this.languageStatusItem);
-			ServerStatusItemFactory.setBusy(this.languageStatusItem);
-		} else {
-			if (this.isAdvancedStatusBarItem) {
-				(this.statusBarItem as any).name = "Java Server Status";
-			}
-			this.statusBarItem.text = StatusIcon.busy;
-			this.statusBarItem.command = Commands.SHOW_SERVER_TASK_STATUS;
-			this.statusBarItem.tooltip = "";
-			this.statusBarItem.show();
-		}
+		this.statusBarItem.name = "Java Server Status";
+		this.statusBarItem.text = StatusIcon.busy;
+		this.statusBarItem.command = Commands.SHOW_SERVER_TASK_STATUS;
+		this.statusBarItem.tooltip = "";
+		this.statusBarItem.show();
 	}
 
 	public setBusy(): void {
-		if (supportsLanguageStatus()) {
-			ServerStatusItemFactory.setBusy(this.languageStatusItem);
-		} else {
-			this.statusBarItem.text = StatusIcon.busy;
-		}
+		this.statusBarItem.text = StatusIcon.busy;
 	}
 
 	public setError(): void {
-		if (supportsLanguageStatus()) {
-			ServerStatusItemFactory.setError(this.languageStatusItem);
-		} else {
-			this.statusBarItem.text = StatusIcon.error;
-			this.statusBarItem.command = Commands.OPEN_LOGS;
-		}
+		this.statusBarItem.text = StatusIcon.error;
+		this.statusBarItem.command = Commands.OPEN_LOGS;
 	}
 
 	public setWarning(): void {
-		if (supportsLanguageStatus()) {
-			ServerStatusItemFactory.setWarning(this.languageStatusItem);
-		} else {
-			this.statusBarItem.text = StatusIcon.warning;
-			this.statusBarItem.command = "workbench.panel.markers.view.focus";
-			this.statusBarItem.tooltip = "Errors occurred in project configurations, click to show the PROBLEMS panel";
-		}
+		this.statusBarItem.text = StatusIcon.warning;
+		this.statusBarItem.command = "workbench.panel.markers.view.focus";
+		this.statusBarItem.tooltip = "Errors occurred in project configurations, click to show the PROBLEMS panel";
 	}
 
 	public setReady(): void {
-		if (supportsLanguageStatus()) {
-			ServerStatusItemFactory.setReady(this.languageStatusItem);
-		} else {
-			this.statusBarItem.text = StatusIcon.ready;
-			this.statusBarItem.command = Commands.SHOW_SERVER_TASK_STATUS;
-			this.statusBarItem.tooltip = "ServiceReady";
-		}
+		this.statusBarItem.text = StatusIcon.ready;
+		this.statusBarItem.command = Commands.SHOW_SERVER_TASK_STATUS;
+		this.statusBarItem.tooltip = "ServiceReady";
 	}
 
 	public updateTooltip(tooltip: string): void {
-		if (!supportsLanguageStatus()) {
-			this.statusBarItem.tooltip = tooltip;
-		}
+		this.statusBarItem.tooltip = tooltip;
 	}
 
 	public dispose(): void {
 		this.statusBarItem?.dispose();
-		this.languageStatusItem?.dispose();
 	}
 }
 
