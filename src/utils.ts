a9144a7fccf01219022e7e7a9e7571c685aef7bb
logonoff <git@logonoff.co>
2024-07-26 15:04:06 -0400

    Clean old autogenerated globalStorage folders (#2597)
    Signed-off-by: logonoff <git@logonoff.co>

--- src/utils.ts
+++ src/utils.ts
@@ -316,3 +316,13 @@ export function resolveActualCause(callstack: any): any {
 
 	return callstack;
 }
+
+export function getVersion(extensionPath: string): string {
+	const packagePath = path.resolve(extensionPath, "package.json");
+	const packageFile = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+	if (packageFile) {
+		return packageFile.version;
+	}
+
+	return '0.0.0';
+}
