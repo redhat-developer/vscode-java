41ab508cc39cb1656382b02706447343fe86b436
Thomas Mäder <t.s.maeder@gmail.com>
2025-09-26 15:14:44 +0200

    Add more telemetry for project import & workspaces hash
    Signed-off-by: Thomas Mäder <t.s.maeder@gmail.com>

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,5 +1,6 @@
 import { TelemetryService, getRedHatService } from "@redhat-developer/vscode-redhat-telemetry";
 import { ExtensionContext, workspace, WorkspaceConfiguration } from "vscode";
+import { cyrb53 } from "./utils";
 
 /**
  * Wrap vscode-redhat-telemetry to suit vscode-java
@@ -10,7 +11,10 @@ export namespace Telemetry {
 	export const COMPLETION_EVENT = "textCompletion";
 	export const SERVER_INITIALIZED_EVT = "java.workspace.initialized";
 	export const LS_ERROR = "java.ls.error";
+	export const IMPORT_PROJECT = "java.workspace.importProject";
+
 	let telemetryManager: TelemetryService = null;
+	let workspaceHash;
 
 	/**
 	 * Starts the telemetry service
@@ -22,6 +26,10 @@ export namespace Telemetry {
 		if (!!telemetryManager) {
 			throw new Error("The telemetry service for vscode-java has already been started");
 		}
+		workspaceHash = cyrb53(workspace.workspaceFolders.map(f => f.uri.toString()).join('|'));
+		workspace.onDidChangeWorkspaceFolders(() => {
+			workspaceHash = cyrb53(workspace.workspaceFolders.map(f => f.uri.toString()).join('|'));
+		});
 		const redhatService = await getRedHatService(context);
 		const telemService = await redhatService.getTelemetryService();
 		telemetryManager = telemService;
@@ -35,54 +43,17 @@ export namespace Telemetry {
 	 * @param data the telemetry data
 	 * @throws Error if the telemetry service has not been started yet
 	 */
-	export async function sendTelemetry(eventName: string, data?: any): Promise<void> {
+	export async function sendTelemetry(eventName: string, data?: object): Promise<void> {
+		console.log(`Sending telemetry event: ${eventName} with data: ${JSON.stringify(data)}`);
 		if (!telemetryManager) {
 			throw new Error("The telemetry service for vscode-java has not been started yet");
 		}
-		const javaSettings = getJavaSettingsForTelemetry(workspace.getConfiguration());
 
-		let properties: any;
-		if (eventName === STARTUP_EVT) {
-			properties= { ...data, ...javaSettings };
-		} else {
-			properties= { ...data};
-		}
-
-		return telemetryManager.send({
+		const event = {
 			name: eventName,
-			properties
-		});
-	}
+			properties:  { workspaceHash, ...data}
+		};
 
-	function getJavaSettingsForTelemetry(config: WorkspaceConfiguration) {
-		// settings whose values we can record
-		const SETTINGS_BASIC = [
-			"java.quickfix.showAt", "java.symbols.includeSourceMethodDeclarations",
-			"java.completion.collapseCompletionItems", "java.completion.guessMethodArguments",
-			"java.cleanup.actionsOnSave", "java.completion.postfix.enabled",
-			"java.sharedIndexes.enabled", "java.inlayHints.parameterNames.enabled",
-			"java.server.launchMode", "java.autobuild.enabled"
-		];
-		// settings where we only record their existence
-		const SETTINGS_CUSTOM = [
-			"java.settings.url", "java.format.settings.url"
-		];
-
-		let value: any;
-		const properties = {};
-
-		for (const key of SETTINGS_CUSTOM) {
-			if (config.get(key)) {
-				properties[key] = true;
-			}
-		}
-		for (const key of SETTINGS_BASIC) {
-			value = config.get(key);
-			if (value !== undefined) {
-				properties[key] = value;
-			}
-		}
-
-		return properties;
+		return telemetryManager.send(event);
 	}
 }
